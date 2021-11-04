4b736f05c8467cee2637bffeb6bbd0117f6b3029
cedar002 <cedarselasi@gmail.com>
2021-11-04 18:04:35 +0000

    Making another push. Still on the project

--- App.js
+++ App.js
@@ -1,13 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "./screens/Login";
+import Home from "./screens/Home";
+// import Cart from "./screens/Cart";
+// import Prod from "./screens/Prod";
+// import bike from "./products/bike";
+// import cloths from "./products/cloths";
+
+
+
+
 
 export default function App() {
+  const MainNavigator = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <View style={{flex: 1}}>
+     <NavigationContainer>
+       <MainNavigator.Navigator screenListeners={{headerShown: false}} initialRouteName="Login">
+         <MainNavigator.Screen name="Home" component={Home}></MainNavigator.Screen>
+         <MainNavigator.Screen name="Login" component={Login}></MainNavigator.Screen>
+         {/* <MainNavigator.Screen name="Cart" component={Cart}></MainNavigator.Screen>
+         <MainNavigator.Screen name="Prod" component={Prod}></MainNavigator.Screen>
+         <MainNavigator.Screen name="bike" component={bike}></MainNavigator.Screen>
+         <MainNavigator.Screen name="cloths" component={cloths}></MainNavigator.Screen> */}
+        
+        
+        
+        
+         
+       </MainNavigator.Navigator>
+     </NavigationContainer>
+   </View>
   );
 }
 
